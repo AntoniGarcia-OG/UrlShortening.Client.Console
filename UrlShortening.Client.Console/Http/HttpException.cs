b5070ed43a6878c8101b74c631971ebc3633bb32
Antoni Ortiz <antonigarcia.og@gmail.com>
2026-01-20 16:40:11 -0600

    Refine HTTP client error handling and configuration setup

--- UrlShortening.Client.Console/Http/HttpException.cs
+++ UrlShortening.Client.Console/Http/HttpException.cs
@@ -4,6 +4,9 @@
     {
         public int StatusCode { get; }
 
-        public HttpException(int statusCode, string message) : base(message) => StatusCode = statusCode;
+        public HttpException(int statusCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+        }
     }
 }
--- UrlShortening.Client.Console/Http/UrlShorteningHttpClient.cs
+++ UrlShortening.Client.Console/Http/UrlShorteningHttpClient.cs
@@ -1,11 +1,24 @@
-﻿namespace UrlShortening.Client.Console.Http
+﻿using System.Net.Http.Json;
+using UrlShortening.Client.Console.DTOs.Requests;
+using UrlShortening.Client.Console.DTOs.Responses;
+
+namespace UrlShortening.Client.Console.Http
 {
     public sealed class UrlShorteningHttpClient
     {
         private readonly HttpClient _httpClient;
 
-        public UrlShorteningHttpClient(HttpClient httpClient) => _httpClient = httpClient;
+        public UrlShorteningHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
 
+        private static async Task<HttpException> CreateHttpException(HttpResponseMessage responseMessage)
+        {
+            string message = await responseMessage.Content.ReadAsStringAsync();
+
+            return new HttpException((int)responseMessage.StatusCode, message);
+        }
     }
 }
